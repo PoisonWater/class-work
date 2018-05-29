87a4229c3b14cb673dbfc906696f9594956cd47d
PoisonWater <117010178@link.cuhk.edu.cn>
2018-05-29 22:54:42 +0800

    Python Summer Review 2

--- CSC1001/Intergrated Functions.py
+++ CSC1001/Intergrated Functions.py
@@ -20,3 +20,55 @@ print([m + n for m in 'ABC' for n in 'XYZ']) # Double
 dictionary = {'x': 'A', 'y': 'B', 'z': 'C' }
 print([k + '=' + v for k, v in dictionary.items()]) # 词典也可使用列表生成器！
 # Result: ['y=B', 'x=A', 'z=C']
+
+### 生成器 Generator
+generator = (x * x for x in range(4))
+print(generator) # Result: <generator object <genexpr> at 0x10463c200> (ID)
+# 可认为生成器Generator为 Queue :
+print(next(generator)) # 0
+print(next(generator)) # 1
+print(next(generator)) # 4
+print(next(generator)) # 9
+# print(next(generator)) # Error.
+
+generator = (x * x for x in range(4))
+# 亦可运用 for loop：
+for n in generator:
+    print(n)
+
+## Eg. Fibonacci Sequence:
+def fib(max):
+    n, a, b = 0, 0, 1
+    while n < max:
+        yield b ## Another method to define Generator.
+        a, b = b, a + b
+        n = n + 1
+    return 'done'
+
+f6 = fib(6)
+for i in f6:
+    print(i) # Result: 1,1,2,3,5,8
+
+f6 = fib(6)
+while True: # Result of this while loop: 
+    try:
+        print("g :", next(f6))
+    except StopIteration as e: # g : 1, g : 1, g : 2, g : 3, g : 5, g : 8, Iteration Done!
+        print("Iteration Done!")
+        break
+
+## Eg2. 生成器执行顺序的理解：
+ # 函数是顺序执行，遇到return语句或者最后一行函数语句就返回。
+ # 生成器Generator，在每次调用next()的时候执行，遇到yield语句返回，再次执行时从上次返回的yield语句处继续执行。
+def odd():
+    print('step 1') 
+    yield 1
+    print('step 2')
+    yield 3
+    print('step 3')
+    yield 5
+
+o = odd()
+print(next(o)) # step1 ; 1
+print(next(o)) # step2 ; 3
+print(next(o)) # step3 ; 5
